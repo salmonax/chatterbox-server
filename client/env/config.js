7433e35c0aa1196f88c2764498cce24d7d7c10e3
salmonax <sarumonakuba@gmail.com>
2016-11-19 11:12:59 -0800

    Remove parse ID

--- client/env/config.js
+++ client/env/config.js
@@ -3,7 +3,7 @@
 
 // Put your parse application keys here!
 //given ID and API key
-$.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', '6UJYuifdHSHnOvG2DiYXU6cwluUvgDiVOpr8Weqi');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'gYu7Z35zwiNz4BaNgwyaq9u9A36eVHd38MiDvCH5');
-});
+// $.ajaxPrefilter(function (settings, _, jqXHR) {
+//   jqXHR.setRequestHeader('X-Parse-Application-Id', '6UJYuifdHSHnOvG2DiYXU6cwluUvgDiVOpr8Weqi');
+//   jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'gYu7Z35zwiNz4BaNgwyaq9u9A36eVHd38MiDvCH5');
+// });
